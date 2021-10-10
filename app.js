ebcc55c7fbbc2807ddbfe9be71c1a2de54fe25de
Ishaan Kulkarni <imkulkarni67@gmail.com>
2021-10-11 00:17:55 +0530

    Update_Commit

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const express=require('express');
 const http=require('http');
 const app=express();
 
-app.listen(5000)
+app.listen(6070)
 
 app.get('/',(req,res)=>{
-    res.sendFile('./docs/index.html',{root:__dirname});
+    res.sendFile('./index.html',{root:__dirname});
 });
 app.get('/about',(req,res)=>{
-    res.sendFile('./docs/about.html',{root:__dirname});
+    res.sendFile('./about.html',{root:__dirname});
 });
 app.get('/contact',(req,res)=>{
-    res.sendFile('./docs/contact.html',{root:__dirname});
+    res.sendFile('./contact.html',{root:__dirname});
 });
 app.get('/OffTopic',(req,res)=>{
-    res.sendFile('./docs/offtopic.html',{root:__dirname});
+    res.sendFile('./offtopic.html',{root:__dirname});
 });
 
 
 app.use((req,res)=>{
-    res.status(404).sendFile('./docs/404.html');
+    res.status(404).sendFile('./404.html');
 })
